ad6e56b6ec461015cacc83331402858009f46563
vood <balitsky_igor@list.ru>
2024-04-07 18:18:33 +0300

    init

--- src/main/kotlin/ru/vood/kora/database/Entity.java
+++ src/main/kotlin/ru/vood/kora/database/Entity.java
@@ -2,4 +2,4 @@ package ru.vood.kora.database;
 
 import ru.tinkoff.kora.database.common.annotation.Column;
 
-record Entity(Long id, @Column("value1") String field1, String value2) {}
+public record Entity(Long id, @Column("value1") String field1, String value2) {}
--- src/main/kotlin/ru/vood/kora/database/EntityRepository1.kt
+++ src/main/kotlin/ru/vood/kora/database/EntityRepository1.kt
@@ -1,14 +1,11 @@
-package ru.vood.kora.database;
+package ru.vood.kora.database
 
-import ru.tinkoff.kora.database.common.annotation.Query;
-import ru.tinkoff.kora.database.common.annotation.Repository;
-import ru.tinkoff.kora.database.jdbc.JdbcRepository;
+import ru.tinkoff.kora.database.common.annotation.Query
+import ru.tinkoff.kora.database.common.annotation.Repository
+import ru.tinkoff.kora.database.jdbc.JdbcRepository
 
 @Repository
-public interface EntityRepository extends JdbcRepository {
-
-
+interface EntityRepository : JdbcRepository {
     @Query("SELECT id, value1, value2 FROM test_table WHERE id = :id")
-    Entity getOneById(Long id);
-
+    fun getOneById(id: Long): Entity
 }
